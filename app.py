da24ce09c29b53c90a0f68616cb64f913a92935f
luangtam <lf631255@gmail.com>
2025-04-27 05:21:56 -0300

    Atividade

--- app.py
+++ app.py
@@ -2,24 +2,42 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# exemplo de uma rota devolvendo apenas um texto.
-@app.route('/', methods=['GET'])
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Rota para a página inicial com o formulário
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Coletar os dados enviados no formulário
+        nome = request.form['nome']
+        formacao = request.form['formacao']
+        curso = request.form['curso']
+        instituicao = request.form['instituicao']
+        ano_formacao = request.form['ano_formacao']
+        experiencia = request.form['experiencia']
+        empresa = request.form['empresa']
+        ano_experiencia = request.form['ano_experiencia']
+        
+        # Passar os dados para a página de resultado
+        return render_template('resultado.html', 
+                               nome=nome, 
+                               formacao=formacao, 
+                               curso=curso, 
+                               instituicao=instituicao, 
+                               ano_formacao=ano_formacao, 
+                               experiencia=experiencia, 
+                               empresa=empresa, 
+                               ano_experiencia=ano_experiencia)
+    
+    # Se for um GET, apenas renderiza a página de entrada
+    return render_template('index.html')
 
-# exemplo de uma rota que devolve um pagina de um template.
-@app.route('/pagina', methods=['GET'])
-def home():
-    return render_template("index.html")
-
-# exemplo de uma rota que trata dados de um formulário html
-@app.route('/novo', methods=['POST'])
-def hello_world_k():  # put application's code here
-    #acessar o BD e salvar essa informação no BD
-    #funcao do banck-end, receber os dados, tratar, fazer validações, pessitir os dados
-    # recuperar dados perssitidos.
-
-    return 'novo PWII' + request.form['nome'] + request.form['aniversario']
+# Rota para a página do autor
+@app.route('/autor')
+def autor():
+    return '''
+        <h1>Autor: Seu Nome</h1>
+        <p>Formações acadêmicas: Curso, Instituição, Ano</p>
+        <p>Experiências profissionais: Função, Empresa, Ano</p>
+    '''
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
